d307c5b8f24ddb79263abca7611fa79e630a6a29
rdlherrera <ralphlherrera@gmail.com>
2025-09-01 21:21:37 +0800

    chore: clear contents of cypress/e2e/pages folder (#14)

--- cypress/e2e/pages/CartPage.js
+++
@@ -1,9 +0,0 @@
-export class CartPage {
-  item(name) {
-    return cy.contains('.cart_item', name); // check if product exists in cart
-  }
-
-  checkout() {
-    return cy.contains('button', 'Checkout').click(); // click checkout button
-  }
-}
